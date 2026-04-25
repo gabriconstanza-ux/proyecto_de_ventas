62015cda43c16464772efb4f45d4f336e3a7944d
gabriela <gabri.constanza@gmail.com>
2026-04-25 19:50:17 -0400

    Corregido

--- logic.py
+++ logic.py
@@ -1,15 +1,15 @@
 def calcular_descuento_usd(monto_usd):
     """Calcula el descuento según los tramos del profesor."""
     if monto_usd > 10000:
-        return monto_usd * 0.13  # 13% de descuento [cite: 5]
+        return monto_usd * 0.13  # 13% de descuento 
     elif monto_usd >= 1000:
-        return monto_usd * 0.10  # 10% de descuento [cite: 5]
-    return 0.0  # Menor a 100 USD es 0% [cite: 5]
+        return monto_usd * 0.10  # 10% de descuento 
+    return 0.0  # Menor a 100 USD es 0% 
 
 def calcular_iva_clp(monto_neto_clp):
     """Aplica el IVA del 19% exigido."""
-    return monto_neto_clp * 0.19  # IVA 19% [cite: 5, 8]
+    return monto_neto_clp * 0.19  # IVA 19% 
 
 def convertir_a_clp(monto_usd, valor_dolar):
     """Convierte USD a CLP según el valor del día."""
-    return monto_usd * valor_dolar [cite: 7]
+    return monto_usd * valor_dolar 
